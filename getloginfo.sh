a1edade607e30fc6c1677bc9ace9a03701a7f33c
sunilka <sunlnx@gmail.com>
2013-08-20 17:35:40 +0530

    Shortening of log file

--- getloginfo.sh
+++ getloginfo.sh
@@ -14,8 +14,8 @@ LOGFILE="/var/log/messages"
 echo "Enter the time stamp to search in log files"
 
 read -p  "Day: " DAY
-read -p  "Month: " MONTH
-read -p  "Hour: " HOUR
+read -p  "Month[Eg aug..etc]: " MONTH
+read -p  "Hour[Eg 02, 10..etc]: " HOUR
 
 echo;
 echo -e "\e[00;31mLogs which occured in mentioned timestamp: $DAY"-"$MONTH"-"$HOUR":00" \e[00m"
