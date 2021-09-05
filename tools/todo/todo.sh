d233d752e37a4dab718eced46365beee5b260c0c
samperay <sunlnx@gmail.com>
2021-09-05 15:37:25 +0530

    done functionality added

--- tools/todo/todo.sh
+++ tools/todo/todo.sh
@@ -2,24 +2,24 @@
 
 todolist="./todolist"
 tododone="./tododone"
-# top_horizontal_line_character="_"
-# bottom_horizontal_line_character='_'
 
 # read options from cli (add,del,done,help)
 readArgs() {
   options="$1"
   case $options in
-    "") tododisplay "$todolist"
-    ;;
     add) shift; todoadd "$@"
-    ;;
-    display)tododisplay "$todolist"
     ;;
     del) shift; tododel "$@"
     ;;
     erase) shift; todoerase "$@"
     ;;
-    help) helpmenu
+    show) tododisplay "$todolist"
+    ;;
+    done) tododonelist "$tododone"
+    ;;
+    help|"-h"|"--help") helpmenu
+    ;;
+    "") tododisplay "$todolist"
     ;;
   esac
 }
@@ -30,6 +30,12 @@ erase() {
 
 countLines() {
   number_of_lines=$(cat $1 | wc -l | tr -d '[:space:]')
+}
+
+# taking n ($2) line out of file ($1) and writing it to $nline
+n-line ()
+{
+	nline=$(sed "${2}q;d" $1)
 }
 
 todoadd() {
@@ -43,45 +49,131 @@ tododisplay() {
   list="$1"
 	countLines "$list"
   if [ $number_of_lines -eq 0 ]; then
-    echo "todo list empty"
+    echo "list's empty"
    exit 0
   fi
-  echo "___"
+  echo "---------"
 	for i in `seq 1 $number_of_lines`
 	do
 		echo "$i. $(sed "${i}q;d" $list)"
 	done
-	echo "___"
+	echo "---------"
 }
 
 todoerase() {
+
+  # write a code to display message for done and todo if not entered.
+  if [ -z "$1" ]; then
+    echo "args needs to be passed (done/todo)"
+    exit 1
+  fi
+
   # erase complete "todo" list
   if [ "$1" = "todo" ]; then
     erase "$todolist"
+    echo "todolist entries has been erased"
   fi
 
   # erase complete "done" task list
   if [ "$1" = "done" ]; then
     erase "$tododone"
+    echo "tododone entries has been erased"
   fi
+}
 
-  echo "$1's entries has been erased"
+####DEL OPTION####
+count-arguments ()
+{
+	nargs=0
+	for n in "$@"
+	do
+		nargs=$((nargs+1))
+	done
+}
+
+construct-array ()
+{
+	i=1
+	for de in "$@"
+  	do
+  		dels[$i]=$de
+  		i=$((i+1))
+  	done
+}
+
+argument-in-array? ()
+{
+	argument=$1
+	switch_aia=false
+	for i in `seq 1 $nargs`
+	do
+		array_value=${dels[i]}
+		if [ $argument = $array_value ]
+		then
+			switch_aia=true
+		fi
+	done
+}
+
+delete-tasks ()
+{
+	touch tmp
+	countLines $todolist
+	for task in `seq 1 $number_of_lines`
+	do
+		argument-in-array? $task
+		if [ "$switch_aia" = "false" ] && [ "$task" -le "$number_of_lines" ] && [ "$task" != "0" ]
+		then
+			n-line $todolist $task
+			echo "$nline" >> tmp
+		fi
+	done
+	echo -n "" > $todolist
+	cat tmp > $todolist
+	rm tmp
+}
+
+append-deleted-tasks-to-done ()
+{
+	countLines $todolist
+	for task in "$@"
+	do
+		if [ "$task" -le "$number_of_lines" ] && [ "$task" != "0" ]
+		then
+			n-line $todolist $task
+			echo "$nline" >> $tododone
+		fi
+	done
+}
+
+tododel(){
+  count-arguments "$@"
+  construct-array "$@"
+  append-deleted-tasks-to-done "$@"
+  delete-tasks
+  echo "Tasks has been deleted"
+}
+
+tododonelist() {
+  tododisplay "$tododone"
 }
 
 helpmenu() {
-printf "This is TODO programm
-Usage: bash todo.sh < |add|del|done|help> < |arg1,arg2,arg2>
-todo.sh - display your todo-list
-todo.sh done - display your done-list
-todo.sh add <entry1,entry2...entryN> - add N entries to your todo-list
-todo.sh del <entry1,entry2...entryN> - delete entries by their order number
-!!! Deleted lines automatically append to your done-list !!!
-todo.sh erase < todo|done > - erase all entries from todolist or donelist
-todo.sh mv <entry1> <entry2>  - change places of entries
-todo.sh stat - print statistics
-todo.sh help - programm's commands and general info (you did that right now)
-"
+printf "TODO Program
+------------
+Usage: ./todo.sh < |add|del|done> < |arg1,arg2,arg2>
+       todo.sh - display your todo-list
+       todo.sh done - display your done-list
+       todo.sh add <entry1,entry2...entryN> - add N entries to your todo-list
+       todo.sh del <entry1,entry2...entryN> - delete entries by their order number
+
+       INFO: !!! Deleted lines automatically append to your done-list !!!
+
+       todo.sh erase < todo|done > - erase all entries from todolist or donelist
+       todo.sh help|-h|--help - programm's commands and general info"
 }
 
 ## Main
 readArgs "$@"
+
+exit 0
